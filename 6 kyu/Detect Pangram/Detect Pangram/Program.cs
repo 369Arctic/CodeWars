b2b261ff9b12ca888ddcd685be1e5542d58f642e
Dmitrii Sannikov <omeom@mail.ru>
2024-08-26 17:52:12 +0500

    Add kyu: Find The Parity Outlier, Split Strings, Detect Pangram. Deleted obj, bin

---
+++ 6 kyu/Detect Pangram/Detect Pangram/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Detect_Pangram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://www.codewars.com/kata/545cedaa9943f7fe7b000048/train/csharp
+
+            Console.WriteLine(IsPangram("qwerty"));
+            Console.WriteLine(IsPangram("qwertyuiop[]asdfghjkl;'zxcvbnm,./"));
+        }
+
+        private static bool IsPangram(string input)
+        {
+            const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            var result = input.ToLower().Where(char.IsLetter).Distinct();
+            return alphabet.All(result.Contains);
+        }
+    }
+}
---
+++ 6 kyu/Split Strings/Split Strings/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Split_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://www.codewars.com/kata/515de9ae9dcfc28eb6000001
+            string[] array1 = SplitStrings("abc");
+            Console.WriteLine(string.Join(", ", array1));
+        }
+
+        private static string[] SplitStrings(string str)
+        {
+            var list = new List<string>();
+
+            for(int i = 0; i < str.Length; i += 2)
+            {
+                if (i + 1 < str.Length)
+                {
+                    list.Add(str.Substring(i, 2));
+                }
+                else
+                {
+                    list.Add(str[i] + "_");
+                }
+
+            }
+            return list.ToArray();
+        }
+    }
+}
